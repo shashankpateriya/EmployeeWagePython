b08e81fd49e175237854e6958acdc46b22aa3f70
Shashank <shashankpateriya4@gmail.com>
2021-09-09 15:10:17 +0530

    [shashank] Add: Computing Employee wage for a month

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -11,22 +11,28 @@ class EmployeeWage:
     full_time_emp_hrs = 8
     half_time_emp_hrs = 4
 
-    def employee_attendance(i):
+    def employee_attendance(self):
         attendance = random.randint(0, 2)
+        if attendance == EmployeeWage.is_full_time:
+            print("Employee is Full time Present with wage " + str(EmployeeWage.full_time_emp_hrs * EmployeeWage.emp_rate_per_hour))
+        elif attendance == EmployeeWage.is_half_time:
+            print("Employee is Half time Present with wage " + str(EmployeeWage.half_time_emp_hrs * EmployeeWage.emp_rate_per_hour))
+        else:
+            print("Employee is absent")
         return attendance
-
-    def employee_work_hours(i, attendance_of_employee):
-        switch = {
-            0: EmployeeWage.full_time_emp_hrs,
-            1: EmployeeWage.half_time_emp_hrs,
-            2: 0
-        }
-        EmployeeWage.emp_hrs = switch.get(attendance_of_employee)
-        print(EmployeeWage.emp_hrs)
 
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
     employee = EmployeeWage()
-    is_present = employee.employee_work_hours(employee.employee_attendance())
-    employee.emp_wage = employee.emp_rate_per_hour * employee.emp_hrs
-    print(employee.emp_wage)
+    total_emp_wage_for_a_month = 0
+    # is_present = employee.employee_attendance()
+    day =1
+    while day<=20:
+        emp_wage_for_a_month = 0
+        print(f"day {day} : ")
+        employee.employee_attendance()
+        emp_wage_for_a_month = employee.full_time_emp_hrs * employee.emp_rate_per_hour
+        total_emp_wage_for_a_month = total_emp_wage_for_a_month + emp_wage_for_a_month
+        day = day + 1
+    else:
+        print(f"\nEmployee's Salary for the Entire Month is: {total_emp_wage_for_a_month}")
