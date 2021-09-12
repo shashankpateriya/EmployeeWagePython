e67fe4e7d1093e72fec9166510b828ff10a35223
Shashank <shashankpateriya4@gmail.com>
2021-09-13 01:08:10 +0530

    [shashank] Add: Using instance variable to save total wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,29 +3,47 @@ import random
 is_absent = 0
 is_half_time = 1
 is_full_time = 2
-emp_rate_per_hour = 20
-emp_hrs = 0
-emp_wage = 0
 full_time_emp_hrs = 8
 half_time_emp_hrs = 4
 
 
-def employee_attendance(wage_per_hour,num_of_working_days,work_hours_per_month,company):
-    attendance = {
-        is_full_time: full_time_emp_hrs,
-        is_half_time: half_time_emp_hrs,
-        is_absent: 0
-    }
-    day = 0
-    total_emp_hours = 0
+class EmpWageBuilder:
+    def __init__(self, wage_per_hour, num_of_working_days, work_hours_per_month, company):
+        self.wage_per_hour = wage_per_hour
+        self.num_of_working_days = num_of_working_days
+        self.work_hours_per_month = work_hours_per_month
+        self.company = company
 
-    while (day < 20 and total_emp_hours < 100):
-        checkemp = random.randint(0, 2)
-        total_emp_hours = total_emp_hours + attendance.get(checkemp)
-        day = day + 1
+    def daily_wage(self,emp_hours):
+        return emp_hours * self.wage_per_hour
 
-    total_emp_wage = total_emp_hours * emp_rate_per_hour
-    print(f"Total wage for company {company} is {total_emp_wage}")
+    def employee_attendance(self,checkemp):
+        attendance = {
+            is_full_time: full_time_emp_hrs,
+            is_half_time: half_time_emp_hrs,
+            is_absent: 0
+        }
+        return attendance.get(checkemp)
 
-employee_attendance(21,25,100,"Unysis")
-employee_attendance(30,35,110,"Airtel")
+    def employee_salary(self):
+        day = 0
+        total_working_hours = 0
+        total_emp_wage = 0
+        while (day < 20 and total_working_hours < 100):
+            checkemp = random.randint(0, 2)
+            emp_hours = self.employee_attendance(checkemp)
+            total_working_hours = total_working_hours + emp_hours
+            day += 1
+            total_emp_wage = self.daily_wage(emp_hours)
+        return total_emp_wage
+
+    def emp_calculate_salary(self):
+        self.total_emp_wage = self.employee_salary()
+        print(f"Employee wage for company {self.company} is {self.total_emp_wage}")
+
+if __name__ == "__main__":
+    print("Welcome to Employee Wage Builder")
+    unysis = EmpWageBuilder(21, 25, 100, "Unysis")
+    airtel = EmpWageBuilder(30, 35, 110, "Airtel")
+    unysis.emp_calculate_salary()
+    airtel.emp_calculate_salary()
